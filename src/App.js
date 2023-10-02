9cdd1f07c71d97165d2aeac5a577d4ee828277ed
Sharath <73765366+sharath617@users.noreply.github.com>
2023-10-02 14:00:47 +0000

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
   const fetchproducts = async () => {
     const res = await fetch("https://dummyjson.com/products?limit=100");
     const data = await res.json();
-    //console.log(data.products);
+    console.log(data.products);
     setProducts(data.products);
   };
 
